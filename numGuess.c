bb33b889a082310aa7a104f0a3d3caf6f7bcc7f7
michaelhchon <mchon@cpp.edu>
2022-10-20 15:43:49 -0700

    created base game for choice 1

--- numGuess.c
+++ numGuess.c
@@ -2,21 +2,43 @@
 //102022
 //Interactive program that allows a user to guess between 1-10
 #include <stdio.h>
+#include <time.h>
 
 int userPrompt();
 
 int main()
 {
     //prompt user for options
+    srand(time(NULL));
     int choice;
+    int userNum;
+    int randNum;
+    int max = 10;
+    int winCon = 1;
     int loopCon = 1;
     do {
         choice = userPrompt();
         //if 1
         if(choice == 1) {
-            //prompt user for number
-            //receive number then tell if correct or wrong
-            //receive new guess until they win
+            randNum = rand() % 10;
+            while(winCon = 1) {
+                //prompt user for number
+                printf("Enter guess from 1 - %d", max);
+                //receive number then tell if correct or wrong
+                scanf("%d", &userNum);
+                //receive new guess until they win
+                if(userNum == randNum) {
+                    printf("%d is the correct number!\n", randNum);
+                    winCon = 0;
+                }
+                else if(userNum > randNum) {
+                    printf("%d is too high! Try again.\n", userNum);
+                }
+                else {
+                    printf("%d is too low! Try again.\n", userNum);
+                }
+            }
+
             //if input is 'q' return to menu
         }
     
