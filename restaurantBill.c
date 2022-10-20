11605b125b14143527861758dcb782db3747d1ee
michaelhchon <mchon@cpp.edu>
2022-10-20 14:51:41 -0700

    created input for argv and receives tip through main arg

--- restaurantBill.c
+++ restaurantBill.c
@@ -6,7 +6,13 @@
 int main(int argc, char *argv[])
 {
     //receive tax (7.25 in ca) and tip
-    double tax, tip;
+    const double tax = 7.25;
+    double tip;
+    if(argc1 != 2) {
+        printf("Usage: %s <tip amount>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    sscanf(arg[1], "%f", &tip);
 
     //const prices for
     //salad - 9.95
@@ -17,6 +23,7 @@ int main(int argc, char *argv[])
     const double soup = 4.55;
     const double sandwich = 13.25;
     const double pizza = 22.35;
+    double total;
 
     //randomly choose between salad, soup, sandwich pizza
     //calculate tax, then tip
