6ac87f5659332a7022bd61c31431e390e6052452
michaelhchon <mchon@cpp.edu>
2022-10-20 14:48:02 -0700

    created variables for tax, tip, salad, soup, sandwich, pizza

--- restaurantBill.c
+++ restaurantBill.c
@@ -3,15 +3,20 @@
 //Computes tax and tip on a restaurant bill for a patron
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     //receive tax (7.25 in ca) and tip
+    double tax, tip;
 
     //const prices for
     //salad - 9.95
     //soup - 4.55
     //sandwich - 13.25
     //pizza - 22.35
+    const double salad = 9.95;
+    const double soup = 4.55;
+    const double sandwich = 13.25;
+    const double pizza = 22.35;
 
     //randomly choose between salad, soup, sandwich pizza
     //calculate tax, then tip
