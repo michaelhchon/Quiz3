55ca458fb5e528564d692669c240aef98651af32
michaelhchon <mchon@cpp.edu>
2022-10-20 14:41:50 -0700

    initial outline for restaurant bill

---
+++ restaurantBill.c
@@ -0,0 +1,22 @@
+//Michael Chon
+//102022
+//Computes tax and tip on a restaurant bill for a patron
+#include <stdio.h>
+
+int main()
+{
+    //receive tax (7.25 in ca) and tip
+
+    //const prices for
+    //salad - 9.95
+    //soup - 4.55
+    //sandwich - 13.25
+    //pizza - 22.35
+
+    //randomly choose between salad, soup, sandwich pizza
+    //calculate tax, then tip
+
+    //display meal cost, tax amount, tip amount, total to output
+
+    return 0;
+}
