48404d5b801d1f51d1ac976cd0f339450fa7b525
michaelhchon <mchon@cpp.edu>
2022-10-20 15:20:11 -0700

    initial outline

--- numGuess.c
+++ numGuess.c
@@ -5,7 +5,18 @@
 
 int main()
 {
-
-
+    //prompt user for options
+    //if 1
+    //prompt user for number
+    //receive number then tell if correct or wrong
+    //receive new guess until they win
+    //if input is 'q' return to menu
     
+    //if 2
+    //provide min and max number
+
+    //if 3
+    // thank user for playing and end game
+
+    return 0;    
 }
