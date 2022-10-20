91b616abc6e84b5b7a9b24a25ba36d40f8f99059
michaelhchon <mchon@cpp.edu>
2022-10-20 15:34:23 -0700

    created loop con and a prompt for user

--- numGuess.c
+++ numGuess.c
@@ -3,20 +3,45 @@
 //Interactive program that allows a user to guess between 1-10
 #include <stdio.h>
 
+int userPrompt();
+
 int main()
 {
     //prompt user for options
-    //if 1
-    //prompt user for number
-    //receive number then tell if correct or wrong
-    //receive new guess until they win
-    //if input is 'q' return to menu
+    int choice;
+    int loopCon = 1;
+    do {
+        choice = userPrompt();
+        //if 1
+        if(choice == 1) {
+            //prompt user for number
+            //receive number then tell if correct or wrong
+            //receive new guess until they win
+            //if input is 'q' return to menu
+        }
     
-    //if 2
-    //provide min and max number
+        //if 2
+        else if(choice == 1) {
+            //provide min and max number
+        }
 
-    //if 3
-    // thank user for playing and end game
+        //if 3
+        else {
+            // thank user for playing and end game
+            printf("Thank you for playing\n");
+            loopCon = 0;
+        }
+    } while(loopCon == 1)
 
     return 0;    
+}
+
+int userPrompt()
+{
+    int choice;
+    printf("Press 1 to play a game\n");
+    printf("Press 2 to change the max number\n");
+    printf("Press 3 to quit\n");
+    scanf("%d", &choice);
+    return choice;
 }
