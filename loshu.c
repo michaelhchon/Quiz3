390cd7190e36d1ec5b1a89981f77888cbd3c7725
michaelhchon <mchon@cpp.edu>
2022-10-20 18:16:23 -0700

    initial file with first ouline

---
+++ loshu.c
@@ -0,0 +1,24 @@
+//Michael Chon
+//102022
+//Program that checks if a 3x3 array is a "Lo Shu Magic Square"
+
+//write function that accepts 2d array and checks if lo shu square
+
+int main()
+{
+    //test with lo shu sqaure
+
+    //test with no lo shu square
+
+    //random 2d array
+    //fill 2d array with numbers 1-9, no repeats
+    //test through function
+
+    //print total number of squares generated and tested before success
+
+    //print square and show placement of values
+
+    return 0;
+}
+
+//function that accepts 2d array and checks if lo shu square
