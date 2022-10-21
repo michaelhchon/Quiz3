6bbaac4334ed1bf99ee38097845aad03205161b5
michaelhchon <mchon@cpp.edu>
2022-10-21 12:34:21 -0700

    third test case with random array

--- loshu.c
+++ loshu.c
@@ -10,30 +10,39 @@
 //write function that accepts 2d array and checks if lo shu square
 int loshu(int arr[R][C]);
 void output(int result, int tries);
+int **fillArray(int x, int y);
+void printSquare(int arr[R][C]);
 
 int main()
 {
     //srand(time(NULL));
     int result;
     //test with lo shu sqaure
-    int arrOne[3][3] = {{4, 9, 2},
+    int arrOne[R][C] = {{4, 9, 2},
                         {3, 5, 7},
                         {8, 1, 6}};
     result = loshu(arrOne);
     output(result, 9);
     //test with no lo shu square
-    int arrTwo[3][3] = {{1, 2, 3},
+    int arrTwo[R][C] = {{1, 2, 3},
                         {4, 5, 6},
                         {7, 8, 9}};
     result = loshu(arrTwo);
     output(result, 9);
-    //random 2d array
-    //fill 2d array with numbers 1-9, no repeats
-    //test through function
 
-    //print total number of squares generated and tested before success
-
-    //print square and show placement of values
+    //fill 2d array with random numbers 1-9, no repeats
+    int arrThree[][];
+    int squares = 0;
+    while(check == 1) {
+        //test through function
+        arrThree = fillArray(R, C);
+        result = loshu(arrThree);
+        squares += 9;
+        output(result, squares);
+        //print square and show placement of values
+        if(result == 1)
+            printSquare(arrThree);
+    }
 
     return 0;
 }
@@ -75,6 +84,10 @@ int loshu(int arr[R][C])
 
 void output(int result, int tries)
 {
+    if(tries > 9) {
+        if(result == 1)
+            printf("Array is a Lo Shu Magic Square. %d squares tested.\n", tries);
+    }
     if(result == 1)
         printf("Array is a Lo Shu Magic Square. %d squares tested.\n", tries);
     else
